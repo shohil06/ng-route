9a552939459fa8612008e38b7211dff667beda1f
shohil06 <shohil.06@gmail.com>
2016-08-08 21:09:54 +0530

    Changes Commited

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,24 +1,21 @@
-
 // create the module and name it SPA1
-var SPA1 = angular.module('SPA1', ['ng-Route']);
+var SPA1 = angular.module('SPA1', ['ngRoute']);
 SPA1.config(function($routeProvider) {
   $routeProvider
-
-  // route for the home page
   .when('/', {
-      templateUrl : 'pages/home.html',
+      templateUrl : '../../pages/home.html',
       controller  : 'mainController'
   })
 
   // route for the about page
   .when('/about', {
-      templateUrl : 'pages/about.html',
+      templateUrl : '../../pages/about.html',
       controller  : 'aboutController'
   })
 
   // route for the contact page
   .when('/contact', {
-      templateUrl : 'pages/contact.html',
+      templateUrl : '../../pages/contact.html',
       controller  : 'contactController'
   });
 });
