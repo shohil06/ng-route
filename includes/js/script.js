a6090403f85fb93641d5cd3a75d385c9704918d2
shohil06 <shohil.06@gmail.com>
2016-08-08 19:29:45 +0530

    changes commited

--- includes/js/script.js
+++ includes/js/script.js
@@ -1,10 +1,36 @@
 
 // create the module and name it SPA1
-var SPA1 = angular.module('SPA1', []);
+var SPA1 = angular.module('SPA1', ['ng-Route']);
+SPA1.config(function($routeProvider) {
+  $routeProvider
+
+  // route for the home page
+  .when('/', {
+      templateUrl : 'pages/home.html',
+      controller  : 'mainController'
+  })
+
+  // route for the about page
+  .when('/about', {
+      templateUrl : 'pages/about.html',
+      controller  : 'aboutController'
+  })
+
+  // route for the contact page
+  .when('/contact', {
+      templateUrl : 'pages/contact.html',
+      controller  : 'contactController'
+  });
+});
 
 // create the controller and inject Angular's $scope
 SPA1.controller('mainController', function($scope) {
-
-    // create a message to display in our view
     $scope.message = 'Everyone come and see how good I look!';
     });
+SPA1.controller('aboutController', function($scope) {
+    $scope.message = 'Look! I am an about page.';
+    });
+SPA1.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+    });
+});
